11420ce02607eb29051f2715ac7583dea65dafef
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-12-19 15:59:01 +0100

    feat: main section 1 left debug

--- src/components/main/MainSection1.vue
+++ src/components/main/MainSection1.vue
@@ -8,15 +8,21 @@ export default {
 
 <template>
     <section>
-        <div>
-            <font-awesome-icon class="prev" :icon="['fas', 'angle-left']" />
-        </div>
-        <div>
-            <font-awesome-icon class="next" :icon="['fas', 'angle-right']" />
-        </div>
-        <div class="cont-sect1">
 
-            <div class="left-section1">aa</div>
+        <div class="cont-sect1">
+            <div>
+                <font-awesome-icon class="prev" :icon="['fas', 'angle-left']" />
+            </div>
+            <div>
+                <font-awesome-icon class="next" :icon="['fas', 'angle-right']" />
+            </div>
+
+            <div class="left-section1">
+                <h1>Devotion that never <span class="bold-text">ends</span></h1>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
+                    non numquam eius modi.</p>
+                <button>Read More</button>
+            </div>
             <div class="right-section1">bb</div>
         </div>
     </section>
@@ -33,15 +39,60 @@ export default {
     border-radius: 50%;
     padding: 5px;
     color: $white;
+
 }
+
+h1 {
+    font-weight: 400;
+}
+
+.next {
+    position: absolute;
+    top: 50%;
+    right: 40px;
+}
+
+.prev {
+    position: absolute;
+    top: 50%;
+    left: 40px;
+}
+
+;
 
 .cont-sect1 {
     display: flex;
     width: 100%;
+    position: relative;
+    height: 500px;
 }
 
 .left-section1,
 .right-section1 {
     width: 50%;
+}
+
+.left-section1 {
+    padding: 110px 50px 160px 450px;
+
+    h1 {
+        font-size: 60px;
+    }
+
+    p {
+        padding: 45px 0;
+    }
+
+    button {
+        border: 1px solid black;
+        padding: 20px 45px;
+        text-transform: uppercase;
+    }
+
+
+}
+
+.bold-text {
+    font-weight: bold;
 }
 </style>
