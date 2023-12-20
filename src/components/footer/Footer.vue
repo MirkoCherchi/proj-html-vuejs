44693239a4b0266ce99d9c0560f9315d30636101
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-12-20 18:16:37 +0100

    feat: footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -3,6 +3,70 @@ export default {};
 
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="cont-footer">
+            <img class="arrow1" src="../../assets/svg/svg-2.svg" alt="">
 
-<style scoped lang="scss"></style>
+
+            <div class="items">
+                <div class="item"><img class="logo" src="/img/logo-sidearea-1.png" alt="logo"><span>Let's Get
+                        Creative</span></div>
+                <div class="item"><span>maree.qode@gmail.com</span><span>+44645 321 789</span></div>
+                <div class="item"><span>Avenue d'Audenghem 10</span><span>1040 Brussels, Belgium</span></div>
+                <div class="item"><span>Stay in touch with us</span>
+                    <ul>
+                        <li><a href="#"></a><font-awesome-icon :icon="['fab', 'twitter']" /></li>
+                        <li><a href="#"></a><font-awesome-icon :icon="['fab', 'pinterest-p']" /></li>
+                        <li><a href="#"></a><font-awesome-icon :icon="['fab', 'facebook-f']" /></li>
+                        <li><a href="#"></a><font-awesome-icon :icon="['fab', 'linkedin-in']" /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/style/typography' as *;
+
+.cont-footer {
+    background-color: $bg-7;
+    padding: 75px 0 90px 0;
+    position: relative;
+
+    .arrow1 {
+        position: absolute;
+        bottom: 35px;
+        right: 35px;
+    }
+}
+
+.items {
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+    width: 1300px;
+    gap: 155px;
+    line-height: 35px;
+
+    .item {
+        display: flex;
+        flex-direction: column;
+
+        .logo {
+            margin-right: 200px;
+            margin-bottom: 10px;
+        }
+
+        ul {
+            display: flex;
+            gap: 30px;
+        }
+
+        li {
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -14,8 +14,23 @@ import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { faCloud } from "@fortawesome/free-solid-svg-icons";
 import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faPinterestP } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 /* add icons to the library */
-library.add(faAngleLeft, faAngleRight, faCircle, faArrowUp, faCloud, faComment);
+library.add(
+  faAngleLeft,
+  faAngleRight,
+  faCircle,
+  faArrowUp,
+  faCloud,
+  faComment,
+  faTwitter,
+  faPinterestP,
+  faFacebookF,
+  faLinkedinIn
+);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
